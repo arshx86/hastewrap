b4000b3c2cab7b97d74fd0b7eeefb7002b9290ab
arsh <85416153+arshx86@users.noreply.github.com>
2022-09-14 20:41:09 +0300

    EndPoints Updated

--- HasteWRAP/Haste.cs
+++ HasteWRAP/Haste.cs
@@ -1,76 +1,49 @@
-﻿using Leaf.xNet;
+﻿#region
+
+using System.IO;
 using System.Linq;
 using System.Text;
+using Leaf.xNet;
+using HttpResponse = Leaf.xNet.HttpResponse;
+using StringContent = Leaf.xNet.StringContent;
+
+#endregion
 
 namespace HasteWRAP
 {
-
-    public class Config
-    {
-        public string Proxy { get; set; }
-        public string UserAgent { get; set; }
-    }
-
     public class HasteBIN
     {
-
-        private readonly bool UseProxy = false;
-        private readonly string ProxyLink;
+        private readonly string Proxy;
         private readonly string UserAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 RuxitSynthetic/1.0 v4354928233897109899 t4157550440124640339";
 
         /// <summary>
-        ///  Initializes a new hastebin wrapper.
+        ///     Initializes a new hastebin wrapper.
         /// </summary>
-        /// <param name="ClientConfig">Custom config for client, contains advanced properties. Leave as null for use default config.</param>
-        public HasteBIN(Config ClientConfig = null)
+        public HasteBIN(string proxy = null)
         {
-
-            if (ClientConfig != null)
-            {
-
-                if (string.IsNullOrEmpty(ClientConfig.Proxy))
-                {
-                    ProxyLink = ClientConfig.Proxy;
-                    UseProxy = true;
-                }
-                if (string.IsNullOrEmpty(ClientConfig.UserAgent))
-                {
-                    UserAgent = ClientConfig.UserAgent;
-                }
-
-            }
-
-
+            Proxy = proxy;
         }
 
         /// <summary>
-        ///  Reads whole content from file, creates a haste.
+        ///     Reads whole content from file, creates a haste.
         /// </summary>
         /// <param name="FilePath">Path of file to read.</param>
         /// <returns>Final result link of haste.</returns>
-        public string CreateHasteFromFile(string FilePath)
+        public string CreateFromFile(string FilePath)
         {
-
-            if (!FilePath.EndsWith(".txt"))
-            {
-                FilePath = $"{FilePath}.txt";
-            }
-
-            string Content = System.IO.File.ReadAllText(FilePath);
-            return CreateHaste(Content);
-
+            string Content = File.ReadAllText(FilePath);
+            return Create(Content);
         }
 
         /// <summary>
-        ///  Reads a text from hastebin link. Useful for directly data reading.
+        ///     Reads a text from hastebin link. Useful for directly data reading.
         /// </summary>
         /// <param name="Code">A full raw link or code.</param>
         /// <returns>Readed data from hastebin.</returns>
-        public string GetHaste(string Code)
+        public string Get(string Code)
         {
-
-            const string Base_ = "https://www.toptal.com/developers/hastebin/raw/";
-            if (Code.Contains("toptal.com")) { Code = Code.Split('/').Last(); }
+            const string Base_ = "https://hastebin.com/raw/";
+            if (Code.Contains("toptal.com")) Code = Code.Split('/').Last();
             string Base = Base_ + Code;
 
             // Initialize clients.
@@ -80,54 +53,39 @@ namespace HasteWRAP
             };
 
             // If user preferred to use proxy, create parser.
-            if (UseProxy)
-            {
-                request.Proxy = HttpProxyClient.Parse(ProxyLink);
-            }
+            if (Proxy != null) request.Proxy = HttpProxyClient.Parse(Proxy);
 
             // Perform actions
             HttpResponse Act = request.Get(Base);
             return Act.ToString();
-
         }
 
         /// <summary>
-        ///  Posts a plain text to hastebin.
+        ///     Posts a plain text to hastebin.
         /// </summary>
         /// <param name="Content">Content to upload.</param>
         /// <returns>URL of the final hastebin result.</returns>
-        public string CreateHaste(string Content)
+        public string Create(string Content)
         {
-
-            const string api_endpoint = "https://www.toptal.com/developers/hastebin/documents";
+            const string api_endpoint = "https://hastebin.com/documents";
             byte[] UploadBytes = Encoding.UTF8.GetBytes(Content);
 
-            HttpRequest req = new HttpRequest
+            HttpRequest req = new Leaf.xNet.HttpRequest
             {
-
-                // Add headers.
                 UserAgent = UserAgent
             };
             req.AddHeader("Content-Length", UploadBytes.Length.ToString());
             req.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
-            // If user preferred to use proxy, create parser.
-            if (UseProxy)
-            {
-                req.Proxy = HttpProxyClient.Parse(ProxyLink);
-            }
+            if (Proxy != null) req.Proxy = HttpProxyClient.Parse(Proxy);
 
-            /* Perform action */
-
+            /* Post */
             HttpResponse act = req.Post(api_endpoint, new StringContent(Content));
             string jsonjunk = act.ToString();
 
             /* Delete JSON Junks, Get Key */
             string GetCode = jsonjunk.Replace("{\"key\":\"", string.Empty).Replace("\"}", string.Empty);
             return $"https://www.toptal.com/developers/hastebin/{GetCode}";
-
         }
-
-
     }
 }
